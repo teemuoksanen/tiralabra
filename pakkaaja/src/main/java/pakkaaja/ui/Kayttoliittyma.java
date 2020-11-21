d18a236a059d3140a908a073ba1e2bbb060ff4ae
Teemu Oksanen <github@teemuoksanen.fi>
2020-11-21 18:39:38 +0200

    Käyttöliittymä refaktoroitu ja lisätty tilasto-ominaisuus

--- pakkaaja/src/main/java/pakkaaja/ui/Kayttoliittyma.java
+++ pakkaaja/src/main/java/pakkaaja/ui/Kayttoliittyma.java
@@ -8,107 +8,162 @@ import pakkaaja.logiikka.Purkaja;
 
 /**
  * Pakkaajan tekstipohjainen käyttöliittymä.
- * @author teemu
  */
 public class Kayttoliittyma {
     
-    private Pakkaaja pakkaaja;
-    private Purkaja purkaja;
+    private Scanner lukija;
+    private boolean tilastot;
+    
+    public Kayttoliittyma() {
+        lukija = new Scanner(System.in);
+        tilastot = false;
+    }
+    
     
     /**
-     * Tekstikäyttöliittymän sisältävä luokka.
+     * Tekstikäyttöliittymän käynnistävä luokka.
      */
     public void kaynnista() {
-        
-        Scanner lukija = new Scanner(System.in);
-        
         System.out.println("TERVETULOA KÄYTTÄMÄÄN PAKKAAJAA!");
         
         while (true) {
-            
-            System.out.println("\nValitse, mitä haluaisit tehdä:\n");
-            System.out.println("[1]\t pakkaa tiedosto");
-            System.out.println("[2]\t pura pakattu tiedosto");
-            System.out.println("[q]\t poistu");
-            System.out.println("");
-            System.out.println("Komento + enter: ");
+            tulostaKomennot();
 
             String komento = lukija.nextLine();
             System.out.println("");
-
-            if (komento.equals("q")) {
-                
-                break;
-                
-            } else if (komento.equals("1")) {
-                
-                String tiedostonimi = "tyhja";
-                File tiedosto = null;
-                
-                while (tiedosto == null || !tiedosto.exists()) {
-                    if (tiedosto != null) {
-                        System.out.println("VIRHE: Tiedostoa '" + tiedostonimi + "' ei löytynyt.\n");
-                    }
-                    
-                    System.out.println("Anna pakattavan tiedoston nimi (ja polku suhteessa ohjelman juurihakemistoon):");
-                    
-                    tiedostonimi = lukija.nextLine();
-                    tiedosto = new File(tiedostonimi);
-                    System.out.println("");
-                }
-                
-                try {
-                    pakkaaja = new Pakkaaja(tiedosto);
-                    System.out.println("Pakataan tiedosto '" + tiedosto.getName() + "'...\n");
-                    File pakattu = pakkaaja.pakkaaTiedosto();
-                    System.out.println("Tiedosto on pakattu ja tallennettu nimellä:");
-                    System.out.println(pakattu.getAbsoluteFile());
-                } catch (Exception ex) {
-                    System.out.println("VIRHE:");
-                    System.out.println(ex.toString());
-                    // ex.printStackTrace();
-                }
-                
-            } else if (komento.equals("2")) {
-
-                String tiedostonimi = "tyhja";
-                File tiedosto = null;
-                
-                while (tiedosto == null || !tiedosto.exists()) {
-                    if (tiedosto != null) {
-                        System.out.println("VIRHE: Pakattua tiedostoa '" + tiedostonimi + "' ei löytynyt.\n");
-                    }
-                    
-                    System.out.println("Anna purettavan tiedoston nimi (ja polku suhteessa ohjelman juurihakemistoon):");
-                    
-                    tiedostonimi = lukija.nextLine();
-                    tiedosto = new File(tiedostonimi);
-                    System.out.println("");
-                }
-                
-                try {
-                    purkaja = new Purkaja(tiedosto);
-                    System.out.println("Puretaan tiedosto '" + tiedosto.getName() + "'...\n");
-                    File purettu = purkaja.puraTiedosto();
-                    System.out.println("Tiedosto on purettu ja tallennettu nimellä:");
-                    System.out.println(purettu.getAbsoluteFile());
-                } catch (Exception ex) {
-                    System.out.println("VIRHE:");
-                    System.out.println(ex.toString());
-                    // ex.printStackTrace();
-                }
-                
-            } else {
-                
-                System.out.println("Virheellinen komento: " + komento);
-                
-            }
-        
+            
+            if (komento.equals("q")) break;
+            else if (komento.equals("1")) pakkaa();
+            else if (komento.equals("2")) pura();
+            else if (komento.equals("3")) vaihdaTilastot();
+            else System.out.println("Virheellinen komento: " + komento);
         }        
         
-        lukija.close();
         System.out.println("Kiitos, kun käytit Pakkaajaa.");
         System.exit(0);
-        
     }
+
+    
+    /**
+     * Ajaa pakaamistoiminnallisuuden.
+     */
+    private void pakkaa() {
+        File tiedosto = kysyTiedostonimi(false);
+        if (tiedosto == null) return;
+
+        try {
+            Pakkaaja pakkaaja = new Pakkaaja(tiedosto);
+            System.out.println("Pakataan tiedosto '" + tiedosto.getName() + "'...\n");
+            long alku = System.nanoTime();
+            File pakattu = pakkaaja.pakkaaTiedosto();
+            long loppu = System.nanoTime();
+            System.out.println("Tiedosto on pakattu ja tallennettu nimellä:");
+            System.out.println(pakattu.getAbsoluteFile());
+            if (tilastot) tulostaTilastot(alku, loppu, tiedosto, pakattu);
+        } catch (Exception ex) {
+            kasittelePoikkeus(ex);
+        }
+    }
+
+    
+    /**
+     * Ajaa purkutoiminnallisuuden.
+     */
+    private void pura() {
+        File tiedosto = kysyTiedostonimi(true);
+        if (tiedosto == null) return;
+
+        try {
+            Purkaja purkaja = new Purkaja(tiedosto);
+            System.out.println("Puretaan tiedosto '" + tiedosto.getName() + "'...\n");
+            long alku = System.nanoTime();
+            File purettu = purkaja.puraTiedosto();
+            long loppu = System.nanoTime();
+            System.out.println("Tiedosto on purettu ja tallennettu nimellä:");
+            System.out.println(purettu.getAbsoluteFile());
+            if (tilastot) tulostaTilastot(alku, loppu, null, null);
+        } catch (Exception ex) {
+            kasittelePoikkeus(ex);
+        }
+    }
+    
+    
+    /**
+     * 
+     */
+    private File kysyTiedostonimi(boolean purkaja) {
+        String tiedostonimi = "";
+        File tiedosto = null;
+
+        while (true) {
+            if (purkaja) System.out.println("Anna purettavan tiedoston nimi (ja polku):");
+            else System.out.println("Anna pakattavan tiedoston nimi (ja polku):");
+            
+            System.out.println("(Jos haluat takaisin päävalikkoon, anna tyhjä nimi.)");
+
+            tiedostonimi = lukija.nextLine();
+            tiedosto = new File(tiedostonimi);
+            System.out.println("");
+            
+            if (tiedostonimi.equals("") || tiedosto == null) return null;
+            else if (!tiedosto.exists()) System.out.println("VIRHE: Tiedostoa '" + tiedostonimi + "' ei löytynyt.\n");
+            else if (tiedosto.isDirectory()) System.out.println("VIRHE: '" + tiedostonimi + "' on hakemisto. Anna yksittäisen tiedoston nimi.\n");
+            else if (!tiedosto.isFile()) System.out.println("VIRHE: '" + tiedostonimi + "' ei ole kelvollinen tiedosto.\n");
+            else return tiedosto;
+        }
+    }
+    
+    /**
+     * Tulostaa tilastot (käytetty aika ja pakkausteho).
+     */
+    private void tulostaTilastot(long alku, long loppu, File tiedosto, File pakattu) {
+        System.out.println("\nTILASTOT:");
+        System.out.println("Aikaa kului: " + (loppu-alku) / 1e9 + " s");
+        if (tiedosto != null) {
+            double pakkaus = 1.0 * pakattu.length() / tiedosto.length();
+            System.out.println("Aluperäinen tiedosto: " + (double) tiedosto.length() / 1024 + " kt");
+            System.out.println("Pakattu tiedosto: " + (double) pakattu.length() / 1024 + " kt");
+            System.out.println("Pakkausteho: " + (double) (1 - pakkaus) * 100 + " %");
+        }
+    }
+    
+    
+    /**
+     * Vaihtaa tilastotulosteet päälle tai pois päältä.
+     */
+    private void vaihdaTilastot() {
+        if (tilastot) {
+            tilastot = false;
+            System.out.println("Tilastotulosteet on kytetty pois päältä.");
+        } else {
+            tilastot = true;
+            System.out.println("Tilastotulosteet on kytetty päälle.");
+            System.out.println("Ohjelma näyttää jatkossa, kuinka nopeasti pakkaaminen ja purkaminen tapahtui, sekä pakatessa pakkaustehon.");
+        }
+    }
+    
+    
+    /**
+     * Käsittelee poikkeuksen.
+     */
+    private void kasittelePoikkeus(Exception ex) {
+        System.out.println("VIRHE:");
+        System.out.println(ex.toString());
+        // ex.printStackTrace();
+    }
+    
+    
+    /**
+     * Tulostaa valittavissa olevat komennot.
+     */
+    private void tulostaKomennot() {
+        System.out.println("\nValitse, mitä haluaisit tehdä:\n");
+        System.out.println("[1]\t pakkaa tiedosto");
+        System.out.println("[2]\t pura pakattu tiedosto");
+        if (tilastot) System.out.println("[3]\t kytke tilastotulosteet pois päältä");
+        else System.out.println("[3]\t kytke tilastotulosteet päälle");
+        System.out.println("[q]\t poistu\n");
+        System.out.println("Komento + enter: ");
+    }
+    
 }
